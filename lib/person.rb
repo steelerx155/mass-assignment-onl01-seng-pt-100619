fb347f6491de3ae80f3e41a0d476826626c88514
Randy Steele <steelerx155@gmail.com>
2019-11-14 19:37:18 +0000

    Done.

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,25 @@
 class Person
-  #your code here
+#your code here
+
+attr_accessor :name, :birthday, :hair_color, :eye_color, :height, :weight, :handed, :complexion, :t_shirt_size, :wrist_size, :glove_size, :pant_length, :pant_width
+
+  def initialize(attributes)
+    # (name:, birthday:, hair_color:, eye_color:, height:, weight:, handed:, complexion:, t_shirt_size:, wrist_size:, glove_size:, pant_length:, pant_width:)
+    @name = name
+    @birthday = birthday
+    @hair_color = hair_color
+    @eye_color = eye_color
+    @height = height
+    @weight = weight
+    @handed = handed
+    @complexion = complexion
+    @t_shirt_size = t_shirt_size
+    @wrist_size = wrist_size
+    @glove_size = glove_size
+    @pant_length = pant_length
+    @pant_width = pant_width
+    
+    attributes.each {|key, value| self.send(("#{key}="), value)}
+  end
+    
 end
