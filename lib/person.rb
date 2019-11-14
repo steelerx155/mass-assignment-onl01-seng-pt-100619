64257fee0c00628e79ff0aaef59372fad23b6471
Randy Steele <steelerx155@gmail.com>
2019-11-14 19:27:07 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -3,5 +3,11 @@ class Person
 attr_accessor :name, :birthday, :hair_color, :eye_color, :height
 
   def initialize(name:, birthday:, hair_color:, eye_color:, height:)
+    @name = name
+    @birthday = birthday
+    @hair_color = hair_color
+    @eye_color = eye_color
+    @height = height
+  end
     
 end
